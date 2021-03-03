7df703ed750bed2cd4f8065cec924e7324c4b68d
Pedro Polanco <pedropolanco02@gmail.com>
2021-03-03 16:06:27 -0500

    post in server created and message input added to client

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 let express = require('express');
+let bodyParser = require("body-parser")
 let app = express();
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+
+let messages = [
+    {name : "pedro", message : "klk"},
+    {name : "goku", message : "kamehameha"}
+]
+
+app.get("/messages",(req,res) => {
+    res.send(messages)
+})
+
+app.post("/messages",(req,res) => {
+    console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
+})
+
 let server = app.listen(3000,() => {
     console.log("server is listening on port " + server.address().port)
 })
